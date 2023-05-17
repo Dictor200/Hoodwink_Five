f7a210a65d7728e96d487eeee55b7fc5fc68d6ae
White_hoodwink <48875064+Dictor200@users.noreply.github.com>
2023-05-17 11:35:18 +0300

    пп

---
+++ hoodwink/hoodwink/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hoodwink
+{
+    internal class Class1
+    {
+        public Class1() {
+            string human;
+            int xp_human = 100;
+            int damage_human = 20;
+            int armor_human = 40;
+      
+        }
+        class Class2
+        {
+            public int MyProperty { get; set; }
+
+            public Class2(int myProperty)
+            {
+                MyProperty = myProperty;
+            }
+
+            public void MyMethod()
+            {
+                Console.WriteLine("Значение поля MyProperty равно " + MyProperty);
+            }
+        }
+
+        string monstr;
+            int xp_monstr = 200;
+            int damage_monstr = 30;
+            int armor_monstr = 0;
+           }
+
+        
+    }
+
---
+++ hoodwink/hoodwink/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hoodwink
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string hello = "Hello";
+            string world = "World";
+            Console.WriteLine(hello + " " + world);
+            Console.ReadKey();
+            int a = 9;
+            int b = 5;
+            int plus = a + b;
+            int minus = a - b;
+            int umnozhenie = a * b;
+            int delenie = a / b;
+            Console.WriteLine("a + b =" + plus);
+            Console.WriteLine("a - b =" + minus);
+            Console.WriteLine("a * b =" + umnozhenie);
+            Console.WriteLine("a / b =" + delenie);
+            Console.ReadKey();
+            Console.WriteLine("Введите ваше имя");
+            string imy = Console.ReadLine();
+            Console.WriteLine("Введете вашу фамиию");
+            string fam = Console.ReadLine();
+            Console.WriteLine("Введите ваше оттчемтво");
+            string och = Console.ReadLine();
+            Console.WriteLine("Ваше полное имя - " + fam + " " + imy + " " + och);
+            Console.ReadKey();
+            int xp_hum = 100;
+            int damage_hum = 40;
+            int armor_hum = 60;
+
+            int xp_mon = 200;
+            int damage_mon = 20;
+            int armor_mon = 1;
+
+            double xp_mon1 = damage_hum / armor_mon;
+            double xp_mon2 = xp_mon - xp_mon1;
+
+            double xp_hum1 = damage_mon / armor_hum;
+            double xp_hum2 = xp_hum - xp_hum1;
+
+            Console.WriteLine("Здоровье человека: " + xp_hum2);
+            Console.WriteLine("Человек нанёс монстру " + xp_hum1 + " урона.");
+
+            Console.WriteLine("Здоровье монстра: " + xp_mon2);
+            Console.WriteLine("Монстр нанёс человеку " + xp_mon1 + " урона.");
+            Console.ReadKey();
+        }
+    }
+}
+
+        
